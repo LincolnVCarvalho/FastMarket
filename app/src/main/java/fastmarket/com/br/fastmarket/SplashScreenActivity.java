3687734c8cb44b34a16f46a638ca7838edae8fe1
LincolnV <liincollnv@hotmail.com>
2018-04-13 17:15:18 -0300

    FM_0.2_Cadastro/Login.xml

--- app/src/main/java/fastmarket/com/br/fastmarket/SplashScreenActivity.java
+++ app/src/main/java/fastmarket/com/br/fastmarket/SplashScreenActivity.java
@@ -1,11 +1,15 @@
 package fastmarket.com.br.fastmarket;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    public static final String LOGIN_PREFERENCE = "INFORMACOES_LOGIN_AUTOMATICO";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,13 +18,28 @@ public class SplashScreenActivity extends AppCompatActivity {
         handle.postDelayed(new Runnable() {
             @Override
             public void run() {
-                mostrarLogin();
+                SharedPreferences prefs = getSharedPreferences(LOGIN_PREFERENCE, MODE_PRIVATE);
+                String login = prefs.getString("login", null);
+
+                if (login!= null) {
+                    // existe configuração salvar
+                } else {
+                    mostrarLogin();
+                }
+
+
             }
         }, 5000);
     }
 
+    private void salvaLogin(){
+        SharedPreferences.Editor editor = getSharedPreferences(LOGIN_PREFERENCE, MODE_PRIVATE).edit();
+        editor.putString("login", "true");
+        editor.commit();
+    }
+
     private void mostrarLogin() {
-        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        Intent intent = new Intent(SplashScreenActivity.this, CadastroActivity.class);
         startActivity(intent);
         finish();
     }
