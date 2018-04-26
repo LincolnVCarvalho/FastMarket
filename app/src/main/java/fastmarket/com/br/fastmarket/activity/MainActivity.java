23dd34a6b2269c57fd9e22006b84b99ba1eccff6
LincolnV <liincollnv@hotmail.com>
2018-04-26 17:12:59 -0300

    NewEditText

--- app/src/main/java/fastmarket/com/br/fastmarket/activity/MainActivity.java
+++ app/src/main/java/fastmarket/com/br/fastmarket/activity/MainActivity.java
@@ -1,17 +1,21 @@
 package fastmarket.com.br.fastmarket.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import fastmarket.com.br.fastmarket.R;
 import fastmarket.com.br.fastmarket.db.Create;
+import fastmarket.com.br.fastmarket.db.MainDB;
 import fastmarket.com.br.fastmarket.helper.Preferencias;
 import fastmarket.com.br.fastmarket.model.Usuario;
 
@@ -19,14 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView teste;
     private Button btnSair;
+    private Button btnTeste;
+    private EditText valida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         teste = findViewById(R.id.txtTeste);
         btnSair = findViewById(R.id.btnSair);
+        btnTeste = findViewById(R.id.btnValidator);
+        valida = findViewById(R.id.txtValida);
 
         Usuario usuario = (Usuario) getIntent().getSerializableExtra("usuarioLogado");
 
@@ -39,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
                 Preferencias preferencias = new Preferencias(MainActivity.this);
                 preferencias.limpaPrefes();
                 finish();
+            }
+        });
+
+        btnTeste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                valida.setError("Easopdkasop");
             }
         });
 
