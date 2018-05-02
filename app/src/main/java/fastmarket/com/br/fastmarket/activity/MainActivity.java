24e405478117e5b0c22aa859ab85c46f77225c17
LincolnV <liincollnv@hotmail.com>
2018-05-02 17:15:17 -0300

    Sliding_InProgress

--- app/src/main/java/fastmarket/com/br/fastmarket/activity/MainActivity.java
+++ app/src/main/java/fastmarket/com/br/fastmarket/activity/MainActivity.java
@@ -4,14 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.zip.Inflater;
 
 import fastmarket.com.br.fastmarket.R;
 import fastmarket.com.br.fastmarket.db.Create;
@@ -21,42 +26,56 @@ import fastmarket.com.br.fastmarket.model.Usuario;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView teste;
-    private Button btnSair;
-    private Button btnTeste;
-    private EditText valida;
+    private Toolbar toolbar;
+    Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        teste = findViewById(R.id.txtTeste);
-        btnSair = findViewById(R.id.btnSair);
-        btnTeste = findViewById(R.id.btnValidator);
-        valida = findViewById(R.id.txtValida);
+        usuario = (Usuario) getIntent().getSerializableExtra("usuarioLogado");
 
-        Usuario usuario = (Usuario) getIntent().getSerializableExtra("usuarioLogado");
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Teste");
+        setSupportActionBar(toolbar);
 
-        if(usuario != null)
-            teste.setText(usuario.toString());
+    }
 
-        btnSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Preferencias preferencias = new Preferencias(MainActivity.this);
-                preferencias.limpaPrefes();
-                finish();
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-        btnTeste.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-                valida.setError("Easopdkasop");
-            }
-        });
+        switch (item.getItemId()){
+            case R.id.item_sair:
+                deslogarUser();
+                return true;
+            case R.id.item_alterardados:
+                atulizaDados(usuario);
+                return true;
+            case R.id.item_historico:
+                Intent intent = new Intent(MainActivity.this, HistoricoActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
+    public void deslogarUser(){
+        Preferencias preferencias = new Preferencias(MainActivity.this);
+        preferencias.limpaPrefes();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void atulizaDados(Usuario usuario){
 
     }
 }
